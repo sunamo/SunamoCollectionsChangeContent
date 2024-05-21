02cb7c898de5ef3d2d44cfecf1fd99e3090c2244
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 20:36:28 +0200

    Everyone on nuget

---
+++ _sunamo/SunamoArgs/ChangeContentArgs.cs
@@ -0,0 +1,9 @@
+namespace SunamoCollectionsChangeContent;
+
+
+public class ChangeContentArgs
+{
+    public bool removeNull = false;
+    public bool removeEmpty = false;
+    public bool switchFirstAndSecondArg = false;
+}
