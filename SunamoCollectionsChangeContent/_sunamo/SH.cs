2384c415680c39d0697db0e8abb53a86d277e388
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 01:04:10 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoCollectionsChangeContent/_sunamo/SH.cs
+++
@@ -1,9 +0,0 @@
-// variables names: ok
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-
-namespace SunamoCollectionsChangeContent._sunamo;
-
-internal class SH
-{
-}
